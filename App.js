40eda18440f44c92db9f9e079b2455569e0eac79
Ajayi Marvellous <66161068+Marvellous890@users.noreply.github.com>
2024-04-01 04:46:15 +0100

    pressable

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-import {View, Button} from 'react-native';
+import {View, Button, Text, Image, Pressable} from 'react-native';
 
 const logoImg = require('./assets/adaptive-icon.png');
 
@@ -10,8 +10,17 @@ export default function App() {
         title="Press"
         onPress={() => console.log("Button Pressed")}
         color="midnightblue"
-        disabled
       />
+      <Pressable onPress={() => console.log("Image Pressed")}>
+        <Image source={logoImg} style={{width: 300, height: 300}}/>
+      </Pressable>
+      <Pressable onPress={() => console.log("Text Pressed")}>
+        <Text>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi a consequat nisl. Vestibulum sed suscipit velit. Etiam lorem nibh, fringilla ut cursus sed, tincidunt id nisl. Aliquam dolor diam, aliquet tempor varius in, convallis non leo. Pellentesque rutrum justo pulvinar, blandit nisi ut, faucibus elit. Donec viverra tortor eu mattis scelerisque. Phasellus luctus elementum sapien ut hendrerit. Quisque tempor neque accumsan aliquet viverra. In a odio ex. Integer sed massa in diam tristique euismod. Quisque vitae varius nunc. Nam at malesuada leo. Cras in viverra mi.
+
+          Vestibulum a semper dolor, ut dictum felis. Phasellus ut dui egestas urna blandit suscipit et at neque. Sed posuere felis in tortor iaculis bibendum semper non sapien. Fusce non sapien aliquam, aliquam leo et, euismod felis. Cras vel magna vel velit rhoncus tincidunt et maximus arcu. Curabitur quam augue, placerat vel luctus sit amet, semper eget nisl. Nunc faucibus condimentum egestas.
+        </Text>
+      </Pressable>
     </View>
   );
 }
