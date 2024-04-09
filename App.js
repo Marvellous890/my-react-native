199a0248c7e3da255503373ac0b6bd125aaca0d6
Ajayi Marvellous <66161068+Marvellous890@users.noreply.github.com>
2024-04-09 10:38:12 +0100

    Stylesheet API

--- App.js
+++ App.js
@@ -1,12 +1,19 @@
-import {View, } from "react-native";
-import Greet from "./components/Greet";
+import {View, Text, StyleSheet } from "react-native";
 
 export default function App() {
 
   return (
-    <View style={{flex: 1, backgroundColor: "plum", padding: 60}}>
-      <Greet name="Bruce Wayne" />
-      <Greet name="Clark Kent" />
+    <View style={styles.container}>
+      <Text style={styles.title}>Stylesheet API</Text>
     </View>
   )
 }
+
+export const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "plum",
+    padding: 60
+  },
+  title: {}
+})
