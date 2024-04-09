d56537f96744c0a4843725b34fc5280f2cd48744
Ajayi Marvellous <66161068+Marvellous890@users.noreply.github.com>
2024-04-09 09:21:49 +0100

    Activity indicator

--- App.js
+++ App.js
@@ -1,13 +1,13 @@
-import {View, StatusBar, Button} from "react-native";
-import {useState} from "react";
+import {View, ActivityIndicator} from "react-native";
 
 export default function App() {
-  const [statusBarVisible, setStatusBarVisible] = useState(true);
 
   return (
     <View style={{flex: 1, backgroundColor: "plum", padding: 60}}>
-      <StatusBar backgroundColor="lightgreen" barStyle="light-content" hidden={statusBarVisible}/>
-      <Button title={"Toggle Status Bar Visibility"} onPress={() => setStatusBarVisible(!statusBarVisible)}/>
+      <ActivityIndicator/>
+      <ActivityIndicator size="large"/>
+      <ActivityIndicator size="large" color="midnightblue"/>
+      <ActivityIndicator size="large" color="midnightblue" animating={false}/>
     </View>
   )
 }
