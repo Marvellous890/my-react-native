04e001ddd1156bf21b3ef4fc9efeff25723c715f
Ajayi Marvellous <ajayimarvellous777@gmail.com>
2024-08-10 20:02:16 +0100

    platform specific code

--- App.js
+++ App.js
@@ -1,4 +1,5 @@
-import {View, StyleSheet, Text, SafeAreaView} from "react-native";
+import {View, StyleSheet, Text, SafeAreaView, Platform} from "react-native";
+import CustomButton from "./components/CustomButton/CustomButton";
 
 export default function App() {
 
@@ -7,6 +8,7 @@ export default function App() {
       <View style={styles.container}>
         <View style={styles.box}>
           <Text style={styles.text}>Welcome</Text>
+          <CustomButton title="Press me" onPress={() => alert("Pressed")}/>
         </View>
       </View>
     </SafeAreaView>
@@ -21,12 +23,23 @@ export const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "plum",
+    paddingTop: Platform.OS === "android" ? 25 : 0
   },
   box: {
     padding:20,
   },
   text: {
-    fontSize: 24,
+    ...Platform.select({
+      ios: {
+        color: "purple",
+        fontSize: 24,
+        fontStyle: "italic"
+      },
+      android: {
+        color: "blue",
+        fontSize: 30
+      }
+    }),
     fontWeight: "bold",
     textAlign: "center",
   }
