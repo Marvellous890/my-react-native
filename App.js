1dae3515a498eac12760094f4786708e3c53af2f
Ajayi Marvellous <ajayimarvellous777@gmail.com>
2024-08-10 19:32:04 +0100

    safe area view

--- App.js
+++ App.js
@@ -1,38 +1,33 @@
-import {View, StyleSheet, Text, useWindowDimensions} from "react-native";
-import {useEffect, useState} from "react";
+import {View, StyleSheet, Text, SafeAreaView} from "react-native";
 
 export default function App() {
-  const windowWidth = useWindowDimensions().width;
-  const windowHeight = useWindowDimensions().height;
 
   return (
-    <View style={styles.container}>
-      <View style={[
-        styles.box, {
-          width: windowWidth > 500 ? "70%" : "90%",
-          height: windowHeight > 600 ? "60%" : "90%"
-        }]}>
-        <Text style={{fontSize: windowWidth > 500 ? 50 : 24}}>Welcome!</Text>
+    <SafeAreaView style={styles.safeContainer}>
+      <View style={styles.container}>
+        <View style={styles.box}>
+          <Text style={styles.text}>Welcome</Text>
+        </View>
       </View>
-    </View>
+    </SafeAreaView>
   )
 }
 
 export const styles = StyleSheet.create({
+  safeContainer: {
+    flex: 1,
+    backgroundColor: "plum",
+  },
   container: {
     flex: 1,
     backgroundColor: "plum",
-    alignItems: "center",
-    justifyContent: "center",
   },
   box: {
-    // width: windowWidth > 500 ? "70%" : "90%",
-    // height: windowHeight > 600 ? "60%" : "90",
-    backgroundColor: "lightblue",
-    alignItems: "center",
-    justifyContent: "center",
+    padding:20,
   },
-  // text: {
-  //   fontSize: windowWidth > 500 ? 50 : 24,
-  // }
+  text: {
+    fontSize: 24,
+    fontWeight: "bold",
+    textAlign: "center",
+  }
 })
