5bbf160e3d492769074e6b28ee68a13a2907d7fe
Ajayi Marvellous <ajayimarvellous777@gmail.com>
2024-11-17 21:28:22 +0100

    TextInput

--- App.js
+++ App.js
@@ -1,55 +1,13 @@
-import {FlatList, SafeAreaView, ScrollView, SectionList, StatusBar, Text, View} from "react-native";
-import pokemonList from './data.json'
-import groupedPokemonList from './grouped-data.json'
+import {SafeAreaView, StatusBar, Text, TextInput} from "react-native";
+import {useState} from "react";
 
 export default function App() {
+  const [name, setName] = useState('')
+
   return (
     <SafeAreaView style={styles.container}>
-      {/*<ScrollView style={styles.scrollView}>
-        {pokemonList.map((pokemon) => (
-          <View style={styles.card} key={pokemon.id}>
-            <Text style={styles.cardText}>{pokemon.type}</Text>
-            <Text style={styles.cardText}>{pokemon.name}</Text>
-          </View>
-        ))}
-      </ScrollView>*/}
-      <View style={styles.scrollView}>
-        {/*<FlatList
-          style={styles.scrollView}
-          data={pokemonList}
-          renderItem={({item}) => {
-            console.log(item.id)
-            return (
-              <View style={styles.card} key={item.id}>
-                <Text style={styles.cardText}>{item.type}</Text>
-                <Text style={styles.cardText}>{item.name}</Text>
-              </View>
-            )
-          }}
-          // horizontal
-          keyExtractor={(item) => item.id.toString()}
-          ItemSeparatorComponent={<View style={{height: 16}}/>}
-          ListEmptyComponent={<Text>No items found</Text>}
-          ListHeaderComponent={<Text style={styles.headerText}>Pokemon Text</Text>}
-          ListFooterComponent={<Text style={styles.footerText}>End of List</Text>}
-        />*/}
-
-        <SectionList
-          sections={groupedPokemonList}
-          renderItem={({item}) => {
-            return (
-              <View style={styles.card}>
-                <Text style={styles.cardText}>{item}</Text>
-              </View>
-            )
-          }}
-          renderSectionHeader={({section}) => {
-            return <Text style={styles.sectionHeaderText}>{section.type}</Text>
-          }}
-          ItemSeparatorComponent={() => <View style={{height: 16}}/>}
-          SectionSeparatorComponent={() => <View style={{height: 16}}/>}
-        />
-      </View>
+      <TextInput style={styles.input} value={name} onChangeText={setName}/>
+      <Text style={styles.text}>My name is: {name}</Text>
     </SafeAreaView>
   )
 }
@@ -57,35 +15,17 @@ export default function App() {
 const styles = {
   container: {
     flex: 1,
-    backgroundColor: '#f5f5f5',
-    paddingTop: StatusBar.currentHeight
+    backgroundColor: '#fff',
+    paddingTop: StatusBar.currentHeight,
   },
-  scrollView: {
-    paddingHorizontal: 16
-  },
-  card: {
-    backgroundColor: "white",
-    padding: 16,
-    borderRadius: 8,
+  input: {
+    height: 40,
+    margin: 12,
+    padding: 10,
     borderWidth: 1,
-    // marginBottom: 16
   },
-  cardText: {
-    fontSize: 30
-  },
-  headerText: {
-    fontSize: 24,
-    textAlign: 'center',
-    marginBottom: 12
-  },
-  footerText: {
-    fontSize: 24,
-    textAlign: 'center',
-    marginTop: 12
-  },
-  sectionHeaderText: {
-    backgroundColor: 'white',
-    fontSize: 24,
-    fontWeight: 'bold',
-  },
+  text: {
+    fontSize:30,
+    padding: 10
+  }
 }
