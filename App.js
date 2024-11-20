3e286a5d85028788ce9de7b02dc32d875aa4abb3
Ajayi Marvellous <ajayimarvellous777@gmail.com>
2024-11-20 13:44:34 +0100

    Bottom Tab navigation options

--- App.js
+++ App.js
@@ -3,15 +3,29 @@ import {NavigationContainer} from "@react-navigation/native";
 import CourseListScreen from "./screens/CourseListScreen";
 import ProfileScreen from "./screens/ProfileScreen";
 import SettingsScreen from "./screens/SettingsScreen";
+import Ionicons from '@expo/vector-icons/Ionicons'
 
 const BottomTabs = createBottomTabNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
-      <BottomTabs.Navigator>
+      <BottomTabs.Navigator
+      screenOptions={{
+        tabBarLabelPosition: "beside-icon",
+        // tabBarShowLabel: false,
+        tabBarActiveTintColor: "purple"
+      }}>
         <BottomTabs.Screen name="Course List" component={CourseListScreen}/>
-        <BottomTabs.Screen name="Profile" component={ProfileScreen}/>
+        <BottomTabs.Screen
+          name="Profile"
+          component={ProfileScreen}
+          options={{
+            tabBarLabel: "My profile",
+            tabBarIcon: ({color}) => <Ionicons name="person" size={20} color={color}/>,
+            tabBarBadge: 3,
+          }}
+        />
         <BottomTabs.Screen name="Settings" component={SettingsScreen}/>
       </BottomTabs.Navigator>
     </NavigationContainer>
